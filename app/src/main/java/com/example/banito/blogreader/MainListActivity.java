aa39a7825d443bf2f9afadcce4347a12e6b8b332
Albano Vaz <albanov94@gmail.com>
2018-01-15 20:09:02 +0200

    Wrong way of fetching data, from the MainThread. Catching the exception with general exception catcher

--- app/src/main/java/com/example/banito/blogreader/MainListActivity.java
+++ app/src/main/java/com/example/banito/blogreader/MainListActivity.java
@@ -2,29 +2,47 @@ package com.example.banito.blogreader;
 
 import android.app.ListActivity;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-import java.lang.reflect.Array;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainListActivity extends ListActivity {
 
-    protected String [] mAndroidNames;
+    protected String [] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
+        // Connecting to the teamthreehouse blog site
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+            //opening the connection with http url connection object
+            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+            connection.connect();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+            //the variable that holds the response from the server
+            int responseCode = connection.getResponseCode();
+            Log.i(TAG, "Code: " + responseCode);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Exception caught", e );
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Exception caught", e );
+        } catch (Exception e){
+            Log.e(TAG, "Exception caught", e );
+        }
 
 
 //        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_SHORT).show();
